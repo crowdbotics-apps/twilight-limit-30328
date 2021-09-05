a03dbee9f734608c536075d9f9c4678c7c455cd5
Crowdbotics <team@crowdbotics.com>
2021-09-05 13:28:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "twilight_limit_30328Identifier",
+  url: "https://twilight_limit_30328Identifier.botics.co",
+  api: "https://twilight_limit_30328Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
